d85f802272c0fc5987715adad6f1bf5fcfe15b08
ANSH-SINGH-2006 <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2020-07-16 16:35:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+//var ball;
+var database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+var allPlayers;
+var distance=0;
+
+var car1,car2,car3,car4,cars;
+var car1_img, car2_img, car3_img, car4_img, track;
+
+function preload(){
+
+    track=loadImage("images/track.jpg");
+    car1_img=loadImage("images/car1.png");
+    car2_img=loadImage("images/car2.png");
+    car3_img=loadImage("images/car3.png");
+    car4_img=loadImage("images/car4.png");
+}
+
+function setup(){
+    createCanvas(displayWidth,displayHeight);
+
+    database=firebase.database();
+    console.log(database);
+
+    game=new Game();
+    game.getState();
+    game.start();
+    
+
+}
+
+
+
+
+function draw(){
+    background("white");
+    if(playerCount===4){
+        game.update(1);
+    }
+
+    if(gameState===1){
+
+        clear();
+        game.play();
+    }
+
+    if(gameState===2){
+
+        game.end();
+    }
+}
+
+
+
+
+
+
+   
+
+
+
+
